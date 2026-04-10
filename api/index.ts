d079f51ddc8aa1f5c2eea0091087f5fd6262d6c4
valb-mig <valb.mig@gmail.com>
2026-04-10 16:48:44 -0300

    chore: vercel

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import { handle } from 'hono/vercel'
 import { app } from '../src/http/app.js'
 
-export const config = { runtime: 'nodejs20.x' }
+export const config = { runtime: 'nodejs' }
 
 export default handle(app)
